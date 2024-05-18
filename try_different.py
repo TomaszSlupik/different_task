02570116217734e7397fe01281a5e5176eda043e
TomaszSlupik <slupiktomasz@gmail.com>
2024-05-18 10:53:08 +0200

    try

--- try_different.py
+++ try_different.py
@@ -10,3 +10,22 @@ except ZeroDivisionError:
 
 
 print('---')
+
+# wydrukować wartość dla klucza: user_id = '004'
+users = {'001': 'Marek', '002': 'Monika', '003': 'Jakub'}
+user_id = '004'
+
+try:
+    myUser = users['004']
+    print(myUser)
+except KeyError:
+    print('Key "004" not found in the dictionary. Adding key...')
+
+users['004'] = None
+
+try:
+    print(users)
+except KeyError:
+    print('Failed')
+
+print('---')
